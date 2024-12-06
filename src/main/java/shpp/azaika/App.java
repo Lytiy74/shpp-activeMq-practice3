1794d88559685b18cfff3449e3a7564d8d886833
Andrew Zaika <lytiy7404@gmail.com>
2024-12-06 11:03:30 +0200

    remove hello world (for test jenkins)

--- src/main/java/shpp/azaika/App.java
+++ src/main/java/shpp/azaika/App.java
@@ -1,13 +1,9 @@
 package shpp.azaika;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
