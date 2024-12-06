0a5de7466bf36814926a0a4d3e673153ffe36b8c
Andrew Zaika <lytiy7404@gmail.com>
2024-12-06 17:33:20 +0200

    add UserPojo

---
+++ src/main/java/shpp/azaika/UserPojo.java
@@ -0,0 +1,46 @@
+package shpp.azaika;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+import shpp.azaika.validation.constraints.CheckEddr;
+
+import java.time.LocalDate;
+
+public class UserPojo {
+    @NotNull
+    @Length(min = 7)
+    @Pattern(regexp = "(?i).*a.*")
+    private String name;
+    @NotNull
+    @CheckEddr(message = "Invalid eddr number")
+    private String eddr;
+    @Min(value = 10)
+    private int count;
+    @NotNull
+    private LocalDate date;
+
+    public UserPojo(String name, String eddr, int count, LocalDate date) {
+        this.name = name;
+        this.eddr = eddr;
+        this.count = count;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEddr() {
+        return eddr;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
