8b5b1a5b235639d1fff8f31a71566dbb1de53583
Andrew Zaika <lytiy7404@gmail.com>
2024-12-09 16:22:47 +0200

    add validation for control number and date

--- src/main/java/shpp/azaika/validation/constraints/constraintvalidators/CheckEddrValidator.java
+++ src/main/java/shpp/azaika/validation/constraints/constraintvalidators/CheckEddrValidator.java
@@ -7,8 +7,9 @@ import shpp.azaika.validation.constraints.CheckEddr;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.stream.IntStream;
 
-public class CheckEddrValidator implements ConstraintValidator<CheckEddr,String> {
+public class CheckEddrValidator implements ConstraintValidator<CheckEddr, String> {
     @Override
     public void initialize(CheckEddr constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
@@ -16,15 +17,29 @@ public class CheckEddrValidator implements ConstraintValidator<CheckEddr,String>
 
     @Override
     public boolean isValid(String eddr, ConstraintValidatorContext context) {
-        return eddr != null && eddr.matches("\\d{8}-\\d{5}") && isDateValid(eddr);
+        return eddr != null && eddr.matches("\\d{8}-\\d{5}") && isDateValid(eddr) && isValidControlNumber(eddr);
     }
 
-    private boolean isDateValid(String eddr) {
+    public boolean isDateValid(String eddr) {
         try {
-        LocalDate.parse(eddr.substring(0,8), DateTimeFormatter.BASIC_ISO_DATE);
-        } catch (DateTimeParseException e){
+            LocalDate date = LocalDate.parse(eddr.substring(0, 8), DateTimeFormatter.BASIC_ISO_DATE);
+            int year = date.getYear();
+            int currentYear = LocalDate.now().getYear();
+            return year > 1900 && year < currentYear;
+        } catch (DateTimeParseException e) {
             return false;
         }
-        return true;
+    }
+
+    //https://blog.uaid.net.ua/ua-id-passport-outside/#google_vignette
+    public boolean isValidControlNumber(String eddr) {
+        int[] weights = new int[]{7, 3, 1, 7, 3, 1, 7, 3, 1, 7, 3, 1};
+        String eddrNumbers = eddr.replace("-", "");
+        int sum = IntStream.range(0, eddrNumbers.length()-1)
+                .map(i -> Character.getNumericValue(eddrNumbers.charAt(i)) * weights[i])
+                .sum();
+        int control = sum % 10;
+        int providedControl = Character.getNumericValue(eddr.charAt(eddr.length() - 1));
+        return control == providedControl;
     }
 }
