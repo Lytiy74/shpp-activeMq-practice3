90eb2a0b5b10042f45919062f28f8cc4036e6507
Andrew Zaika <lytiy7404@gmail.com>
2024-12-11 17:54:23 +0200

    temp changes of main

--- src/main/java/shpp/azaika/App.java
+++ src/main/java/shpp/azaika/App.java
@@ -7,12 +7,13 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.util.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import shpp.azaika.util.PropertyManager;
-import shpp.azaika.util.UserPojoGenerator;
+import shpp.azaika.pojo.UserPojo;
+import shpp.azaika.util.*;
 import shpp.azaika.util.mq.Consumer;
 import shpp.azaika.util.mq.Producer;
 
 import javax.jms.JMSException;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -20,25 +21,28 @@ public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(args[0]);
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        int sendedMessages = 0;
 
         PropertyManager propertyManager = new PropertyManager("app.properties");
-        long timeForGenerationInSec = Integer.parseInt(propertyManager.getProperty("generation.duration"));
         String userName = propertyManager.getProperty("activemq.user");
         String userPassword = propertyManager.getProperty("activemq.pwd");
         String urlMq = propertyManager.getProperty("activemq.url");
         String destinationName = propertyManager.getProperty("activemq.queue");
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(userName, userPassword, urlMq);
+        long timeForGenerationInSec = Integer.parseInt(propertyManager.getProperty("generation.duration"));
 
+        int n = Integer.parseInt(args[0]);
         StopWatch stopWatch = new StopWatch(true);
+
+        producer(n, connectionFactory, destinationName, stopWatch, timeForGenerationInSec);
+
+        consumer(connectionFactory, destinationName, stopWatch);
+    }
+
+    private static void producer(int n, ActiveMQConnectionFactory connectionFactory, String destinationName, StopWatch stopWatch, long timeForGenerationInSec) throws JsonProcessingException {
+        int sendedMessages = 0;
         try (Producer producer = new Producer(connectionFactory)) {
             producer.connect(destinationName);
-            for (int i = 0; i < n && stopWatch.taken() < timeForGenerationInSec; i++) {
+            for (int i = 0; i < n && TimeUnit.SECONDS.convert(stopWatch.taken(),TimeUnit.MILLISECONDS) < timeForGenerationInSec; i++) {
                 String userPojoJsonString = getUserPojoJsonString();
                 producer.sendTextMessage(userPojoJsonString);
                 sendedMessages++;
@@ -53,13 +57,30 @@ public class App {
         logger.info("WASTED TIME {}s", wastedTimeInSeconds);
         logger.info("TOTAL SEND MESSAGES  {}", sendedMessages);
         logger.info("SENDING MESSAGE PER SECOND {}", sendingMps);
+    }
 
-        try (Consumer consumer = new Consumer(connectionFactory, "valid.csv", "invalid.csv")) {
+    private static void consumer(ActiveMQConnectionFactory connectionFactory, String destinationName, StopWatch stopWatch) throws FileNotFoundException {
+        long wastedTimeInSeconds;
+        int receivedMessages = 0;
+        double receivingMps = 0;
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        MessageHandler messageHandler = new MessageHandler(mapper, new UserValidation(), new CsvWriter("valid.csv"), new CsvWriter("invalid.csv"));
+        try (Consumer consumer = new Consumer(connectionFactory, messageHandler)) {
             consumer.connect(destinationName);
-            consumer.start();
+            stopWatch.restart();
+            while (true) {
+                boolean processed = consumer.processNextMessage();
+                if (!processed) break;
+                receivedMessages++;
+            }
         } catch (JMSException e) {
             throw new RuntimeException(e);
         }
+        wastedTimeInSeconds = TimeUnit.SECONDS.convert(stopWatch.stop(), TimeUnit.MILLISECONDS);
+        receivingMps = ((double)receivedMessages/wastedTimeInSeconds);
+        logger.info("WASTED TIME {}s", wastedTimeInSeconds);
+        logger.info("TOTAL RECEIVED MESSAGES  {}", receivedMessages);
+        logger.info("RECEIVING MESSAGE PER SECOND {}", receivingMps);
     }
 
     private static String getUserPojoJsonString() throws JsonProcessingException {
