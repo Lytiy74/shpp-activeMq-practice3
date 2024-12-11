a3cf07db701190756171fd69e595278f70186c50
Andrew Zaika <lytiy7404@gmail.com>
2024-12-11 17:54:53 +0200

    refactored Consumer

--- src/main/java/shpp/azaika/util/mq/Consumer.java
+++ src/main/java/shpp/azaika/util/mq/Consumer.java
@@ -1,54 +1,29 @@
 package shpp.azaika.util.mq;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import shpp.azaika.UserPojo;
+import shpp.azaika.util.MessageHandler;
 
 import javax.jms.*;
-import java.io.*;
-import java.util.Set;
 
 public final class Consumer implements AutoCloseable {
 
     private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
-    private final ObjectMapper objectMapper;
     private final ActiveMQConnectionFactory connectionFactory;
-    private final Validator validator;
-    private final CsvMapper csvMapper;
+
     private Connection connection;
     private Session session;
     private MessageConsumer messageConsumer;
-    private boolean isRunning;
+    private final MessageHandler messageHandler;
 
-    private final String validFileName;
-    private final String invalidFileName;
-
-    private final BufferedOutputStream outputStreamForValid;
-    private final BufferedOutputStream outputStreamForInvalid;
-
-    public Consumer(ActiveMQConnectionFactory connectionFactory, String validFileName, String invalidFileName) throws FileNotFoundException {
+    public Consumer(ActiveMQConnectionFactory connectionFactory, MessageHandler messageHandler) {
         if (connectionFactory == null) {
             throw new IllegalArgumentException("ConnectionFactory must not be null");
         }
+        this.messageHandler = messageHandler;
         this.connectionFactory = connectionFactory;
-        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
-        this.isRunning = true;
-        this.validFileName = validFileName;
-        this.invalidFileName = invalidFileName;
-        this.csvMapper = (CsvMapper) new CsvMapper().registerModule(new JavaTimeModule());
-        this.objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        this.outputStreamForInvalid = new BufferedOutputStream(new FileOutputStream(validFileName, true));
-        this.outputStreamForValid = new BufferedOutputStream(new FileOutputStream(invalidFileName, true));
-
         logger.debug("Consumer initialized");
     }
 
@@ -72,56 +47,24 @@ public final class Consumer implements AutoCloseable {
         return session.createConsumer(destination);
     }
 
-    public void start() throws JMSException {
-        connection.start();
+    public boolean processNextMessage() {
         try {
-            while (isRunning) {
-                Message receive = messageConsumer.receive();
-                if (receive != null) messageHandler(receive);
+            Message message = messageConsumer.receive();
+            if (message == null) return false;
+
+            if (messageHandler.isPoisonPill(message)) {
+                logger.info("Received poison pill, stopping processing.");
+                return false;
             }
+
+            messageHandler.handleMessage(message);
+            return true;
         } catch (Exception e) {
-            logger.error("Error while consuming messages", e);
+            logger.error("Error processing message", e);
+            return false;
         }
     }
 
-    private void messageHandler(Message message) throws JMSException, IOException {
-        if (message instanceof TextMessage) {
-            String text = ((TextMessage) message).getText();
-            if (text.equals(Producer.POISON_PILL)) {
-                isRunning = false;
-                return;
-            }
-            validateMessage(text);
-        } else {
-            logger.warn("Received unsupported message type: {}", message.getClass().getSimpleName());
-        }
-    }
-
-    private void validateMessage(String text) throws IOException {
-        try {
-            UserPojo userPojo = mapTextToUserPojo(text);
-            Set<ConstraintViolation<UserPojo>> violations = validator.validate(userPojo);
-
-            if (violations.isEmpty()) {
-                logger.info("Write {} to file {}",userPojo, validFileName);
-                writeToFile(outputStreamForValid, userPojo);
-            } else {
-                logger.info("Write {} to file {}",userPojo, invalidFileName);
-                writeToFile(outputStreamForInvalid, userPojo);
-            }
-        } catch (JsonProcessingException e) {
-            logger.error("Failed to parse JSON message: {}", text, e);
-        }
-    }
-
-    private void writeToFile(OutputStream outputStream, UserPojo userPojo) throws IOException {
-        CsvSchema schema = csvMapper.schemaFor(UserPojo.class);
-        csvMapper.writer(schema).writeValue(outputStream, userPojo);
-    }
-
-    private UserPojo mapTextToUserPojo(String text) throws JsonProcessingException {
-        return objectMapper.readValue(text, UserPojo.class);
-    }
 
     @Override
     public void close() throws JMSException {
