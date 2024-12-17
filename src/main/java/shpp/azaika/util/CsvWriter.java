34d805c78e3259daa26026e66d957f367580542d
Andrew Zaika <lytiy7404@gmail.com>
2024-12-17 18:09:51 +0200

    reworked csvwriter added buffered writer

--- src/main/java/shpp/azaika/util/CsvWriter.java
+++ src/main/java/shpp/azaika/util/CsvWriter.java
@@ -3,33 +3,45 @@ package shpp.azaika.util;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import shpp.azaika.pojo.UserPojo;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CsvWriter implements AutoCloseable {
     private static final Logger logger = LoggerFactory.getLogger(CsvWriter.class);
     private final CsvMapper csvMapper;
-    private final FileOutputStream outputStream;
+    private final BufferedOutputStream outputStream;
+    private final CsvSchema schema;
+    private final ObjectWriter writer;
 
-    public CsvWriter(String fileName) throws FileNotFoundException {
-        this.outputStream = new FileOutputStream(fileName, true);
+    public CsvWriter(String fileName) throws IOException {
+        this.outputStream = new BufferedOutputStream(new FileOutputStream(fileName, true), 16384);
         this.csvMapper = (CsvMapper) new CsvMapper().registerModule(new JavaTimeModule());
         this.csvMapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
+
+        this.schema = csvMapper.schemaFor(UserPojo.class).withColumnSeparator(',');
+        this.writer = csvMapper.writer(schema);
+
         logger.info("CsvWriter initialized for file: {}", fileName);
     }
 
     public void write(UserPojo userPojo) throws IOException {
-        CsvSchema schema = csvMapper.schemaFor(UserPojo.class).withColumnSeparator(',');
-        csvMapper.writer(schema).writeValue(outputStream, userPojo);
+        writer.writeValue(outputStream, userPojo);
     }
 
     @Override
     public void close() throws IOException {
-        outputStream.close();
-        logger.info("CsvWriter closed");
+        try {
+            outputStream.flush();
+        } finally {
+            outputStream.close();
+            logger.info("CsvWriter closed");
+        }
     }
 }
